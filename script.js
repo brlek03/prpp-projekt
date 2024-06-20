84982bf70ba3bd9b23135eb9cba5a66530aa2a1e
brlek03 <brlek.jurica@gmail.com>
2024-06-20 23:52:02 +0200

    Created index, style and search bar js function.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('search-icon').addEventListener('click', function() {
+    var searchBar = document.getElementById('search-bar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
+    }
+});
