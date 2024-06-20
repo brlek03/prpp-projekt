d7095f97798dbed893f6ee8450d2c96670725966
brlek03 <brlek.jurica@gmail.com>
2024-06-21 01:14:16 +0200

    Added novosti, studentski poslovi, studiji to index.html

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.getElementById('search-icon').addEventListener('click', function() {
         searchBar.style.display = 'none';
     }
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); 
+}
